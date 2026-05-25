5a2499044ec8d2a8f90f269dac7171c8f9a6addf
Michael Lieberman <michaellieberman44@gmail.com>
2026-05-25 13:59:04 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Header from './Header'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="main">
+        <div className="nav-bar">
+          <Header />
+        </div>
+      </section>
+
+    </>
+  )
+}
+
+export default App
