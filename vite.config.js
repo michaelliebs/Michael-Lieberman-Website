8d226e1545e5d6da03cd4f16c1b2576870677b71
Michael Lieberman <michaellieberman44@gmail.com>
2026-05-25 20:25:36 -0400

    deleted base line

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base:'michael-lieberman-website',
+  // base:'michael-lieberman-website',
   plugins: [react()],
 })
