19ff65d40d8fa6e76a52719f57742cc654805954
Michael Lieberman <michaellieberman44@gmail.com>
2026-05-31 12:47:32 -0400

    Changed to BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './stylesheets/index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 )
